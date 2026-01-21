edd35ccef9fd5ddaa7318eabcea32abebc61f1fd
Julian Ortlieb <mygames@julian-ortlieb.de>
2026-01-21 21:29:23 +0000

    Refactor code structure for improved readability and maintainability

--- install/opendesk-install.sh
+++ install/opendesk-install.sh
@@ -31,11 +31,11 @@ msg_ok "Installed Dependencies"
 # =============================================================================
 
 msg_info "Installing k3s (Lightweight Kubernetes)"
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --disable traefik --write-kubeconfig-mode 644" sh -
+$STD curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --disable traefik --write-kubeconfig-mode 644" sh -
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >>/root/.bashrc
+$STD echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >>/root/.bashrc
 sleep 10
-kubectl wait --for=condition=Ready nodes --all --timeout=120s
+$STD kubectl wait --for=condition=Ready nodes --all --timeout=120s
 msg_ok "Installed k3s"
 
 # =============================================================================
@@ -43,22 +43,22 @@ msg_ok "Installed k3s"
 # =============================================================================
 msg_info "Installing Helm, Helmfile, Helmdiff, Cert-Manager, Ingress-NGINX"
 # Helm
-curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4 | bash
+$STD curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-4 | bash
 # Helmfile
-HELMFILE_VERSION=$(curl -s "https://api.github.com/repos/helmfile/helmfile/releases/latest" | jq -r .tag_name)
-curl "https://github.com/helmfile/helmfile/releases/download/${HELMFILE_VERSION}/helmfile_linux_amd64.tar.gz" -o /tmp/helmfile_linux_amd64.tar.gz
-tar -xzf /tmp/helmfile_linux_amd64.tar.gz -C /tmp
+HELMFILE_VERSION=$($STD curl -s "https://api.github.com/repos/helmfile/helmfile/releases/latest" | jq -r .tag_name)
+$STD curl "https://github.com/helmfile/helmfile/releases/download/${HELMFILE_VERSION}/helmfile_linux_amd64.tar.gz" -o /tmp/helmfile_linux_amd64.tar.gz
+$STD tar -xzf /tmp/helmfile_linux_amd64.tar.gz -C /tmp
 mv /tmp/helmfile /usr/local/bin/helmfile
 chmod +x /usr/local/bin/helmfile
 rm /tmp/helmfile_linux_amd64.tar.gz
 
 # Helmdiff
-helm plugin install https://github.com/databus23/helm-diff --verify=false
+$STD helm plugin install https://github.com/databus23/helm-diff --verify=false
 
 # Cert-Manager cert-manager.io
-helm repo add jetstack https://charts.jetstack.io --force-update
-helm repo update
-helm install \
+$STD helm repo add jetstack https://charts.jetstack.io --force-update
+$STD helm repo update
+$STD helm install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
@@ -66,13 +66,13 @@ helm install \
   --set crds.enabled=true
 
 # Ingress-NGINX
-helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx --force-update
-helm repo update
-helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
+$STD helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx --force-update
+$STD helm repo update
+$STD helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
   --namespace ingress-nginx --create-namespace \
   --set controller.service.type=LoadBalancer \
   --set controller.watchIngressWithoutClass=true
-kubectl wait --namespace ingress-nginx --for=condition=available deployment/ingress-nginx-controller --timeout=120s
+$STD kubectl wait --namespace ingress-nginx --for=condition=available deployment/ingress-nginx-controller --timeout=120s
 
 # =============================================================================
 # INSTALL OPENDESK VIA HELMFILE
@@ -80,8 +80,10 @@ kubectl wait --namespace ingress-nginx --for=condition=available deployment/ingr
 msg_info "Installing openDesk"
 WORKDIR="/opt/opendesk"
 mkdir -p $WORKDIR
-git clone https://gitlab.opencode.de/bmi/opendesk/deployment/opendesk.git $WORKDIR
-cd $WORKDIR
+$STD git clone https://gitlab.opencode.de/bmi/opendesk/deployment/opendesk.git $WORKDIR
+cd $WORKDIR/ || exit
+
+
 
 # =============================================================================
 # Finishing Up
