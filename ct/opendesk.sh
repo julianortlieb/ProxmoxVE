5a17c618a0f00b884fce24d2564481adfb4d51fc
Julian <61797558+julianortlieb@users.noreply.github.com>
2026-01-22 13:56:42 +0100

    Update opendesk.sh

--- ct/opendesk.sh
+++ ct/opendesk.sh
@@ -13,7 +13,7 @@ var_ram="${var_ram:-16384}"
 var_disk="${var_disk:-15}"
 var_os="${var_os:-debian}"
 var_version="${var_version:-13}"
-var_unprivileged="${var_unprivileged:-1}"
+var_unprivileged="${var_unprivileged:-0}"
 
 header_info "$APP"
 variables
